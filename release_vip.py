b27a04b51ea36a242edb02c8a991d23db5846bb2
sszgr <sszgr@outlook.com>
2017-10-27 16:11:23 +0800

    Update VIP -> VIPId
    API_DescribeVIP response=>{  ..  "VIPSet":[{  ..  "VIPId":"vip-mvstds" }] }

--- release_vip.py
+++ release_vip.py
@@ -6,12 +6,12 @@ from config import *
 import sys
 import json
 
-#实例化 API 句柄
+# 实例化 API 句柄
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     arg_length = len(sys.argv)
     ApiClient = UcloudApiClient(base_url, public_key, private_key)
-    Parameters={"Action":"ReleaseVIP","VIP":"10.10.3.13","Region":"cn-north-03"}
-    response = ApiClient.get("/", Parameters);
-    print response;
+    Parameters = {"Action": "ReleaseVIP", "VIPId": "vip-abcdef", "Region": "cn-bj2"}
+    response = ApiClient.get("/", Parameters)
+    print response
